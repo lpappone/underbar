b5c8c9cb73378e264490e3cb36df89a8c479591b
Lauren Pappone <lpappone@gmail.com>
2014-05-29 16:36:42 -0700

    Update underbar.js

--- src/underbar.js
+++ src/underbar.js
@@ -38,7 +38,9 @@ var _ = {};
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
-    return n === undefined ? array.pop() : array.slice(array.length - n);
+    if(n>array.length) {return array}
+    else {
+    return n === undefined ? array.pop() : array.slice(array.length - n)};
   };
 
   // Call iterator(value, key, collection) for each element of collection.
