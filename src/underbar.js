3329387daa16bb0c79037207e8b021e76c10d159
lpappone <lpappone@gmail.com>
2014-05-30 15:25:00 -0700

    'reject'

--- src/underbar.js
+++ src/underbar.js
@@ -96,6 +96,13 @@ var _ = {};
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var results = [];
+  	_.each(collection, function(item) {
+  		if(test(item) === false) {
+  			results.push(item)
+  		}
+  	})
+  	return results;
   };
 
   // Produce a duplicate-free version of the array.
