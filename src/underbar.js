796936c6578647af27caed6fbc5c38061283008f
lpappone <lpappone@gmail.com>
2014-05-30 12:26:02 -0700

    'each'

--- src/underbar.js
+++ src/underbar.js
@@ -52,12 +52,12 @@ var _ = {};
   _.each = function(collection, iterator) {
   	if(Array.isArray(collection)) {
   		for (var i = 0; i < collection.length; i++) {
-  			iterator(collection[i], key, collection);
+  			iterator(collection[i], i, collection);
         }
   	}
   	else {
   		for (var i in collection) {
-  			iterator(collection[i], key, collection);
+  			iterator(collection[i], i, collection);
   		}                         
 
   	}
