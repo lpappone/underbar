199914d3531282b7344afea774bd2a011869a246
lpappone <lpappone@gmail.com>
2014-05-30 12:23:49 -0700

    'each

--- src/underbar.js
+++ src/underbar.js
@@ -8,6 +8,7 @@ var _ = {};
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+  	return val;
   };
 
   /**
@@ -49,6 +50,18 @@ var _ = {};
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+  	if(Array.isArray(collection)) {
+  		for (var i = 0; i < collection.length; i++) {
+  			iterator(collection[i], key, collection);
+        }
+  	}
+  	else {
+  		for (var i in collection) {
+  			iterator(collection[i], key, collection);
+  		}                         
+
+  	}
+
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
